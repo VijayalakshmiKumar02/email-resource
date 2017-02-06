fea6c442eac4e28b293de0428e6cb2092934e923
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-02-06 10:38:05 +0530

    Update email.sh

--- ci/email.sh
+++ ci/email.sh
@@ -5,5 +5,5 @@ set -exu
 
 output_body_file=email-out/$output_body_file
 output_subject_file=email-out/$output_subject_file
-echo -e "success" > $output_subject_file
-echo -e "deoployed successfully" > $output_body_file
+echo  "success" > $output_subject_file
+echo  "deoployed successfully" > $output_body_file
