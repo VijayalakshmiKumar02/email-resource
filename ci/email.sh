28a11d0c747a09913955dfcba3fe16609c01c3e0
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-02-08 12:59:29 +0530

    Update email.sh

--- ci/email.sh
+++ ci/email.sh
@@ -5,5 +5,8 @@ set -exu
 
 output_body_file=email-out/$output_body_file
 output_subject_file=email-out/$output_subject_file
-echo  "success" > $output_subject_file
-echo  "deoployed successfully" > $output_body_file
+echo  "SUCCESS" > $output_subject_file
+echo  "The app has been successfully pushed to the cf.Please refer to the link for more info. 
+        http://sonarqube.mvp2.cognizantone.org/about/dashboard/index/com.cognizant.deployprocessorSupport:DeployProcessorSupportTool
+        http://sonarqube.mvp2.cognizantone.org/about/dashboard/index/com.cognizant.starterkit:sk
+        http://sonarqube.mvp2.cognizantone.org/about/dashboard/index/com.cognizant.deployprocessor:DeployProcessor" > $output_body_file
