69a085de2d8678335a45e30472c83a72a56a4ba3
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-02-08 14:34:02 +0530

    Update email_failure.sh

--- ci/email_failure.sh
+++ ci/email_failure.sh
@@ -5,5 +5,7 @@ set -exu
 
 output_body_file=email-out/$output_body_file
 output_subject_file=email-out/$output_subject_file
-echo "failure" > $output_subject_file
-echo  "Test cases failure" > $output_body_file
+echo " BUILD FAILURE" > $output_subject_file
+echo  "Please find the link for more info
+        
+       http://52.220.34.126:8080/teams/main/pipelines/blueprint" > $output_body_file
